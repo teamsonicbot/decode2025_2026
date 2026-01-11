992bf19f438a9fac4d390d5208846dd1788d420a
teamsonicbot <ftc31944@gmail.com>
2026-01-10 20:25:06 -0500

    Add files via upload
    Qualifier II Moorsefield

---
+++ BLUE_1_JAVA.java
@@ -0,0 +1,301 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+
+@Autonomous(name = "BLUE_1_JAVA", preselectTeleOp = "TELEOP_JAVA")
+public class BLUE_1_JAVA extends LinearOpMode {
+
+
+    // ===== DRIVE =====
+    private DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor;
+
+
+    // ===== SHOOTER =====
+    private DcMotorEx leftLauncher, rightLauncher;
+    private Servo angleChanger;
+
+
+    // ===== INTAKE =====
+    private DcMotor intakeMotor;
+    private CRServo rightIntake, leftIntake;
+
+
+    // ===== FAN =====
+    private CRServo fan;
+
+
+    // ===== VOLTAGE =====
+    private VoltageSensor battery;
+
+
+    // ===== PF CONSTANTS =====
+    private static final double kP = 0.0009;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+    private static final double kF = 18.0;
+    private static final double NOMINAL_VOLTAGE = 12.0;
+
+
+    // ===== ENCODER =====
+    private static final double TICKS_PER_REV = 28.0;
+
+
+    // ===== TELEOP SHOOTER PRESETS =====
+    private static final double CLOSE_RPM = 2700;
+    private static final double MED_RPM   = 2000;  // NEW VERSION ONLY fOR THIS CODE
+    private static final double FAR_RPM   = 2100;
+    private static final double EXTRA_FAR_RPM = 2511;
+
+
+    private static final double CLOSE_ANG = 0.25;
+    private static final double MED_ANG   = 0.169;  // NEW VERSION ONLY fOR THIS CODE
+    private static final double FAR_ANG   = 0.15;
+    private static final double EXTRA_FAR_ANG = 0.16;
+
+
+    @Override
+    public void runOpMode() {
+
+
+        // ===== DRIVE MAP =====
+        frontLeftMotor  = hardwareMap.get(DcMotor.class, "front left");
+        backLeftMotor   = hardwareMap.get(DcMotor.class, "back left");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "front right");
+        backRightMotor  = hardwareMap.get(DcMotor.class, "back right");
+
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        // ===== SHOOTER MAP =====
+        leftLauncher  = hardwareMap.get(DcMotorEx.class, "left launcher");
+        rightLauncher = hardwareMap.get(DcMotorEx.class, "right launcher");
+
+
+        leftLauncher.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightLauncher.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        leftLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        // ===== INTAKE MAP =====
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake");
+        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        rightIntake = hardwareMap.get(CRServo.class, "right intake");
+        leftIntake  = hardwareMap.get(CRServo.class, "Leftintake");
+
+
+        leftIntake.setDirection(CRServo.Direction.FORWARD);
+        rightIntake.setDirection(CRServo.Direction.REVERSE);
+
+
+        fan = hardwareMap.get(CRServo.class, "blue fan");
+
+
+        // ===== ANGLE =====
+        angleChanger = hardwareMap.get(Servo.class, "Angle Changer");
+        angleChanger.setPosition(MED_ANG); // TeleOp MED preset
+
+
+        // ===== VOLTAGE =====
+        battery = hardwareMap.voltageSensor.iterator().next();
+
+
+        telemetry.addLine("Autonomous Ready");
+        telemetry.update();
+
+
+        waitForStart();
+
+
+        if (opModeIsActive()) {
+
+//Position shot
+            movingForward(0.6, 1300);
+            strafing(0.7, -200);
+
+//Shoot
+            sleep(3);
+            shootMED();
+            sleep(3500);
+
+ //Moving to first set artifacts
+            strafing(0.8, 200);
+            rotating(0.8, 1500);
+            stopLauncher();
+            strafing(0.8, -880);
+
+ //Picking up artifacts
+            startIntakes();
+            movingForward(0.6, 1400);
+            movingForward(0.8, -1400);
+
+// Position shot
+            strafing(0.8,300);
+            rotating(0.8,-1450);
+            movingForward(0.6, -100);
+            strafing(0.8, -200);
+
+ //Shoot
+            shootMED();
+            sleep(3000);
+            stopLauncher();
+
+//Moving to next set of artifacts of TeleOp
+            strafing(0.8, 200);
+            rotating(0.8,1450);
+            strafing(0.8,-1511);
+
+ //The following code can be used or not bc it shoots extra set
+
+            movingForward(0.2, 1600);
+            movingForward(0.6, -1600);
+            strafing(0.6, 1300);
+            rotating(0.5,-1300);
+            strafing(0.7,-200);
+ //Shoot
+            shootMED();
+            sleep(3500);
+            stopLauncher();
+        }
+    }
+
+
+    // ===== DRIVE FUNCTIONS =====
+    private void movingForward(double speed, int distance) {
+        resetAndRunToPosition(distance, distance, distance, distance, speed);
+    }
+
+
+    private void strafing(double speed, int distance) {
+        resetAndRunToPosition(distance, -distance, -distance, distance, speed);
+    }
+
+
+    private void rotating(double speed, int distance) {
+        resetAndRunToPosition(distance, -distance, distance, -distance, speed);
+    }
+
+
+    private void resetAndRunToPosition(int fl, int fr, int bl, int br, double speed) {
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        frontLeftMotor.setTargetPosition(fl);
+        frontRightMotor.setTargetPosition(fr);
+        backLeftMotor.setTargetPosition(bl);
+        backRightMotor.setTargetPosition(br);
+
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+
+        frontLeftMotor.setPower(speed);
+        frontRightMotor.setPower(speed);
+        backLeftMotor.setPower(speed);
+        backRightMotor.setPower(speed);
+
+
+        while (opModeIsActive()
+                && frontLeftMotor.isBusy()
+                && frontRightMotor.isBusy()
+                && backLeftMotor.isBusy()
+                && backRightMotor.isBusy()) {
+            idle();
+        }
+
+
+        frontLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        backRightMotor.setPower(0);
+    }
+
+
+    // ===== SHOOTER FUNCTIONS LIKE TELEOP =====
+    private void shootMED() {
+        double targetTPS = rpmToTicksPerSecond(MED_RPM);
+        double voltage = battery.getVoltage();
+        if (voltage <= 0) voltage = NOMINAL_VOLTAGE;
+        double compensatedF = kF * (NOMINAL_VOLTAGE / voltage);
+
+
+        leftLauncher.setVelocityPIDFCoefficients(kP, kI, kD, compensatedF);
+        rightLauncher.setVelocityPIDFCoefficients(kP, kI, kD, compensatedF);
+
+
+        leftLauncher.setVelocity(targetTPS);
+        rightLauncher.setVelocity(targetTPS);
+
+
+        angleChanger.setPosition(MED_ANG);
+
+
+        intakeMotor.setPower(1.0);
+        leftIntake.setPower(1.0);
+        rightIntake.setPower(1.0);
+        fan.setPower(1.0);
+    }
+
+
+    private void stopLauncher() {
+        leftLauncher.setVelocity(0);
+        rightLauncher.setVelocity(0);
+
+
+        intakeMotor.setPower(0);
+        leftIntake.setPower(0);
+        rightIntake.setPower(0);
+        fan.setPower(0);
+    }
+
+
+    private void startIntakes() {
+        intakeMotor.setPower(1.0);
+        leftIntake.setPower(1.0);
+        rightIntake.setPower(1.0);
+    }
+
+
+    private void stopIntakes() {
+        intakeMotor.setPower(0.0);
+        leftIntake.setPower(0.0);
+        rightIntake.setPower(0.0);
+    }
+
+
+    private double rpmToTicksPerSecond(double rpm) {
+        return rpm * TICKS_PER_REV / 60.0;
+    }
+}
+
+
+
+
+
---
+++ BLUE_2_JAVA.java
@@ -0,0 +1,331 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+@Autonomous(name = "BLUE_2_JAVA", preselectTeleOp = "TELEOP_JAVA")
+public class BLUE_2_JAVA extends LinearOpMode {
+
+    private DcMotor backleft;
+    private DcMotor backright;
+    private DcMotor frontleft;
+    private DcMotor frontright;
+    private DcMotor intake;
+    private DcMotorEx leftlauncher;
+    private DcMotorEx rightlauncher;
+    private CRServo bluefan;
+    private CRServo Leftintake;
+    private CRServo rightintake;
+    private Servo AngleChanger;
+    private VoltageSensor battery;
+
+    // ===== SHOOTER CONSTANTS =====
+    static final double TICKS_PER_REV = 28.0;
+    static final double CLOSE_RPM = 3000;
+    static final double MED_RPM = 3000;
+    static final double FAR_RPM = 2100;
+    static final double EXTRA_FAR_RPM = 2795;
+    static final double CLOSE_ANG = 0.25;
+    static final double MED_ANG = 0.20;
+    static final double FAR_ANG = 0.15;
+    static final double EXTRA_FAR_ANG = 0.134;
+    static final double kP = 0.0009;
+    static final double kF = 18.0;
+    static final double NOMINAL_VOLTAGE = 12.0;
+
+    @Override
+    public void runOpMode() {
+        int speed;
+        int distance;
+        int speed_left;
+        int speed_right;
+        int strafeleft;
+        int straferight;
+        int seconds;
+
+        backleft = hardwareMap.get(DcMotor.class, "back left");
+        backright = hardwareMap.get(DcMotor.class, "back right");
+        frontleft = hardwareMap.get(DcMotor.class, "front left");
+        frontright = hardwareMap.get(DcMotor.class, "front right");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        leftlauncher = hardwareMap.get(DcMotorEx.class, "left launcher");
+        rightlauncher = hardwareMap.get(DcMotorEx.class, "right launcher");
+        bluefan = hardwareMap.get(CRServo.class, "blue fan");
+        Leftintake = hardwareMap.get(CRServo.class, "Leftintake");
+        rightintake = hardwareMap.get(CRServo.class, "right intake");
+        AngleChanger = hardwareMap.get(Servo.class, "Angle Changer");
+        battery = hardwareMap.voltageSensor.iterator().next();
+
+        // Put initialization blocks here.
+        backleft.setTargetPosition(0);
+        backright.setTargetPosition(0);
+        frontleft.setTargetPosition(0);
+        frontright.setTargetPosition(0);
+        backleft.setPower(0);
+        frontright.setPower(0);
+        frontleft.setPower(0);
+        backright.setPower(0);
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intake.setDirection(DcMotor.Direction.REVERSE);
+        leftlauncher.setDirection(DcMotor.Direction.FORWARD);
+        rightlauncher.setDirection(DcMotor.Direction.REVERSE);
+        bluefan.setDirection(CRServo.Direction.FORWARD);
+        Leftintake.setDirection(CRServo.Direction.FORWARD);
+        rightintake.setDirection(CRServo.Direction.REVERSE);
+        AngleChanger.setPosition(0.22);
+        backleft.setDirection(DcMotor.Direction.REVERSE);
+        frontleft.setDirection(DcMotor.Direction.REVERSE);
+        frontright.setDirection(DcMotor.Direction.FORWARD);
+        backright.setDirection(DcMotor.Direction.FORWARD);
+        backleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftlauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightlauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontleft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontright.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // SHOOTER SETUP
+        applyPF();
+
+        speed_left = 0;
+        speed_right = 0;
+        strafeleft = 0;
+        straferight = 0;
+        distance = 0;
+        speed = 0;
+        seconds = 0;
+        waitForStart();
+        if (opModeIsActive()) {
+            // Put loop blocks here.
+            moving_backward(0.3, 220);
+            rotate(0.3, -223);
+            Intake();
+
+            //FIRST SHOT
+            setExtraFarShot();
+            bluefan.setPower(1);
+            sleep(3000);
+            bluefan.setPower(0);
+
+            rotate(0.6, 1131);
+            strafing_right(0.5, 950);
+            sleep(1000);
+            moving_forward_(0.25, 1600);
+            sleep(1000);
+            moving_backward(0.5, 1500);
+            strafing_right(0.4, -860);
+            rotate(0.5, -1220);
+            moving_forward_(0.5, 150);
+
+            //SECOND SHOT
+            setExtraFarShot();
+            bluefan.setPower(1);
+            sleep(3000);
+            bluefan.setPower(0);
+
+            rotate(0.3, 1185);
+            strafing_right(0.5, 1850);
+
+        }
+    }
+
+    // ===== YOUR ORIGINAL FUNCTIONS (UNCHANGED) =====
+    private void turn_some_random_shit(double speed, int distance) {
+        backleft.setTargetPosition(distance);
+        backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backleft.setPower(speed);
+        backright.setTargetPosition(distance);
+        backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backright.setPower(0);
+        frontleft.setTargetPosition(distance);
+        frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontleft.setPower(speed);
+        frontright.setTargetPosition(distance);
+        frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontright.setPower(0);
+        while (backleft.isBusy() && backright.isBusy() && frontleft.isBusy() && frontright.isBusy()) {
+        }
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backleft.setPower(0);
+        backright.setPower(0);
+        frontleft.setPower(0);
+        frontright.setPower(0);
+        telemetry.update();
+    }
+
+    private void moving_forward_(double speed, int distance) {
+        backleft.setTargetPosition(distance);
+        backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backleft.setPower(speed);
+        backright.setTargetPosition(distance);
+        backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backright.setPower(speed);
+        frontleft.setTargetPosition(distance);
+        frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontleft.setPower(speed);
+        frontright.setTargetPosition(distance);
+        frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontright.setPower(speed);
+        while (backleft.isBusy() && backright.isBusy() && frontleft.isBusy() && frontright.isBusy()) {
+        }
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backleft.setTargetPosition(0);
+        backright.setTargetPosition(0);
+        frontright.setTargetPosition(0);
+        frontleft.setTargetPosition(0);
+        backleft.setPower(0);
+        backright.setPower(0);
+        frontleft.setPower(0);
+        frontright.setPower(0);
+        telemetry.update();
+    }
+
+    private void moving_backward(double speed, int distance) {
+        backleft.setTargetPosition(-distance);
+        backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backleft.setPower(speed);
+        backright.setTargetPosition(-distance);
+        backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backright.setPower(speed);
+        frontleft.setTargetPosition(-distance);
+        frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontleft.setPower(speed);
+        frontright.setTargetPosition(-distance);
+        frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontright.setPower(speed);
+        while (backleft.isBusy() && backright.isBusy() && frontleft.isBusy() && frontright.isBusy()) {
+        }
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backleft.setPower(0);
+        backright.setPower(0);
+        frontleft.setPower(0);
+        frontright.setPower(0);
+        telemetry.update();
+    }
+
+    private void strafing_right(double speed, int distance) {
+        backleft.setTargetPosition(-distance);
+        backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backleft.setPower(speed);
+        backright.setTargetPosition(distance);
+        backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backright.setPower(speed);
+        frontleft.setTargetPosition(distance);
+        frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontleft.setPower(speed);
+        frontright.setTargetPosition(-distance);
+        frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontright.setPower(speed);
+        while (backleft.isBusy() && backright.isBusy() && frontleft.isBusy() && frontright.isBusy()) {
+        }
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backleft.setPower(0);
+        backright.setPower(0);
+        frontleft.setPower(0);
+        frontright.setPower(0);
+        telemetry.update();
+    }
+
+    private void Intake() {
+        intake.setPower(0.8);
+        Leftintake.setPower(1);
+        rightintake.setPower(1);
+    }
+
+    private void Intake2() {
+        intake.setPower(0);
+        Leftintake.setPower(0);
+        rightintake.setPower(0);
+    }
+
+    private void rotate(double speed, int distance) {
+        backleft.setTargetPosition(distance);
+        backleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backleft.setPower(speed);
+        backright.setTargetPosition(-distance);
+        backright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backright.setPower(speed);
+        frontleft.setTargetPosition(distance);
+        frontleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontleft.setPower(speed);
+        frontright.setTargetPosition(-distance);
+        frontright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontright.setPower(speed);
+        while (backleft.isBusy() && backright.isBusy() && frontleft.isBusy() && frontright.isBusy()) {
+        }
+        backleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontleft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontright.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backleft.setPower(0);
+        backright.setPower(0);
+        frontleft.setPower(0);
+        frontright.setPower(0);
+        telemetry.update();
+    }
+
+    // ===== SHOOTER FUNCTIONS =====
+    double rpmToTicksPerSecond(double rpm) {
+        return rpm * TICKS_PER_REV / 60.0;
+    }
+
+    void applyPF() {
+        double voltage = battery.getVoltage();
+        if (voltage <= 0) voltage = NOMINAL_VOLTAGE;
+        double compensatedF = kF * (NOMINAL_VOLTAGE / voltage);
+        leftlauncher.setVelocityPIDFCoefficients(kP, 0.0, 0.0, compensatedF);
+        rightlauncher.setVelocityPIDFCoefficients(kP, 0.0, 0.0, compensatedF);
+    }
+
+    void setCloseShot() {
+        double tps = rpmToTicksPerSecond(CLOSE_RPM);
+        leftlauncher.setVelocity(tps);
+        rightlauncher.setVelocity(tps);
+        AngleChanger.setPosition(CLOSE_ANG);
+    }
+
+    void setMediumShot() {
+        double tps = rpmToTicksPerSecond(MED_RPM);
+        leftlauncher.setVelocity(tps);
+        rightlauncher.setVelocity(tps);
+        AngleChanger.setPosition(MED_ANG);
+    }
+
+    void setFarShot() {
+        double tps = rpmToTicksPerSecond(FAR_RPM);
+        leftlauncher.setVelocity(tps);
+        rightlauncher.setVelocity(tps);
+        AngleChanger.setPosition(FAR_ANG);
+    }
+
+    void setExtraFarShot() {
+        double tps = rpmToTicksPerSecond(EXTRA_FAR_RPM);
+        leftlauncher.setVelocity(tps);
+        rightlauncher.setVelocity(tps);
+        AngleChanger.setPosition(EXTRA_FAR_ANG);
+    }
+}
